b323ba8cffcbe7ac767ac842026b7b0948e4bbb7
amitxv <amitxv@users.noreply.github.com>
2023-02-02 14:20:47 +0000

    Refactor code, add type hints

--- src/service-list-builder.py
+++ src/service-list-builder.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+from typing import Any, List, Tuple, Union
 import winreg
 import os
 import sys
@@ -8,41 +8,12 @@ import ctypes
 import win32con
 import win32service
 
-class_hive = "SYSTEM\\CurrentControlSet\\Control\\Class"
-services_hive = "SYSTEM\\CurrentControlSet\\Services"
+
+def split_lines(array: List[str]) -> str:
+    return "\\0".join(array)
 
 
-def parse_config(section, array_name, cfg):
-    for i in cfg[section]:
-        if i != "" and i not in array_name:
-            array_name.append(i)
-
-
-def append_filter(filter_name, filter_type, arr_name):
-    key_data = []
-    with winreg.OpenKey(
-        winreg.HKEY_LOCAL_MACHINE,
-        f"{class_hive}\\{filter_name}",
-        0,
-        winreg.KEY_READ | winreg.KEY_WOW64_64KEY,
-    ) as key:
-        key_data = winreg.QueryValueEx(key, filter_type)[0]
-        for i in arr_name:
-            if i in key_data:
-                key_data.remove(i)
-    return split_lines(key_data)
-
-
-def split_lines(arr_name):
-    string = ""
-    for i in arr_name:
-        string += i
-        if i != arr_name[-1]:
-            string += "\\0"
-    return string
-
-
-def read_value(path, value_name):
+def read_value(path: str, value_name: str) -> Union[Tuple[Any, int], None]:
     try:
         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path, 0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY) as key:
             try:
@@ -53,8 +24,19 @@ def read_value(path, value_name):
         return None
 
 
-def main():
+def main() -> None:
     version = "0.3.6"
+
+    filter_dict = {
+        "{4d36e967-e325-11ce-bfc1-08002be10318}": {"LowerFilters"},
+        "{71a27cdd-812a-11d0-bec7-08002be2092f}": {"LowerFilters", "UpperFilters"},
+        "{4d36e96c-e325-11ce-bfc1-08002be10318}": {"UpperFilters"},
+        "{6bdd1fc6-810f-11d0-bec7-08002be2092f}": {"UpperFilters"},
+        "{ca3e7ab9-b4c3-4ae6-8251-579ef933890f}": {"UpperFilters"},
+    }
+
+    class_hive = "SYSTEM\\CurrentControlSet\\Control\\Class"
+    services_hive = "SYSTEM\\CurrentControlSet\\Services"
 
     print(f"service-list-builder v{version}")
     print("GitHub - https://github.com/amitxv\n")
@@ -87,85 +69,78 @@ def main():
     config.optionxform = str  # type: ignore
     config.read(args.config)
 
-    automatic = []
-    manual = []
-    service_dump = []
-    rename_folders_executables = []
+    automatic = set(service for service in config["Automatic_Services"] if service)
+    manual = set(service for service in config["Manual_Services"] if service)
+    service_dump = set(driver for driver in config["Drivers_To_Disable"] if driver)
+    rename_binaries = set(binary for binary in config["Toggle_Files_Folders"] if binary)
 
-    parse_config("Automatic_Services", automatic, config)
-    parse_config("Manual_Services", manual, config)
-    parse_config("Drivers_To_Disable", service_dump, config)
-    parse_config("Toggle_Files_Folders", rename_folders_executables, config)
+    statuses = win32service.EnumServicesStatus(win32service.OpenSCManager(None, None, win32con.GENERIC_READ))
 
-    statuses = win32service.EnumServicesStatus(win32service.OpenSCManager(None, None, win32con.GENERIC_READ))  # type: ignore
-
-    if len(automatic) > 0 or len(manual) > 0:
-        for (service_name, _desc, _status) in statuses:
+    if automatic or manual:
+        service_name: str
+        for (service_name, _, _) in statuses:  # TODO: populate list manually by looping through keys
+            # remove _XXXXX user services id
             if "_" in service_name:
-                svc, _, _suffix = service_name.rpartition("_")
-                service_name = svc
-            if service_name not in service_dump:
-                service_dump.append(service_name)
+                service_name = service_name.rpartition("_")[0]
+
+            service_dump.add(service_name)
 
     service_dump = sorted(service_dump, key=str.lower)
 
-    filter_dict = {
-        "{4d36e967-e325-11ce-bfc1-08002be10318}": {"LowerFilters": ["EhStorClass"]},
-        "{71a27cdd-812a-11d0-bec7-08002be2092f}": {
-            "LowerFilters": ["fvevol", "iorate", "rdyboost"],
-            "UpperFilters": ["volsnap"],
-        },
-        "{4d36e96c-e325-11ce-bfc1-08002be10318}": {"UpperFilters": ["ksthunk"]},
-        "{6bdd1fc6-810f-11d0-bec7-08002be2092f}": {"UpperFilters": ["ksthunk"]},
-        "{ca3e7ab9-b4c3-4ae6-8251-579ef933890f}": {"UpperFilters": ["ksthunk"]},
-    }
-
-    ds_lines = []
-    es_lines = []
+    # store contents of batch scripts
+    ds_lines: List[str] = []
+    es_lines: List[str] = []
 
     ds_lines.append("@echo off")
     es_lines.append("@echo off")
 
-    for item in rename_folders_executables:
-        if os.path.exists(item):
-            file_name = os.path.basename(item)
-            last_index = item[-1]
-            ds_lines.append(f'REN "{item}" "{file_name}{last_index}"')
-            es_lines.append(f'REN "{item}{last_index}" "{file_name}"')
+    for binary in rename_binaries:
+        if os.path.exists(binary):
+            file_name: str = os.path.basename(binary)
+            last_index = binary[-1]  # .exe gets renamed to .exee
+            ds_lines.append(f'REN "{binary}" "{file_name}{last_index}"')
+            es_lines.append(f'REN "{binary}{last_index}" "{file_name}"')
         else:
-            print(f"info: item does not exist: {item}")
+            # TODO: ask user if they want to disable it anyway and argument to supress all choices
+            print(f"info: item does not exist: {binary}")
 
-    for filter_name in filter_dict:
-        for filter_type in filter_dict[filter_name]:
-            if read_value(f"{class_hive}\\{filter_name}", filter_type) is not None:
-                for driver in filter_dict[filter_name][filter_type]:
+    for filter_id, filter_types in filter_dict.items():  # TODO: loop through keys instead of hardcoding them
+        for filter_type in filter_types:
+            original: List[str] = read_value(f"{class_hive}\\{filter_id}", filter_type)  # type: ignore
+
+            # check if the filter exists
+            if original is not None:
+                new = original.copy()  # to keep a backup of the original
+                for driver in original:
                     if driver in service_dump:
-                        ds_value = append_filter(filter_name, filter_type, service_dump)
-                        ds_lines.append(
-                            f'reg.exe add "HKLM\\{class_hive}\\{filter_name}" /v "{filter_type}" /t REG_MULTI_SZ /d "{ds_value}" /f'
-                        )
-                        es_value = split_lines(read_value(f"{class_hive}\\{filter_name}", filter_type))  # type: ignore
-                        es_lines.append(
-                            f'reg.exe add "HKLM\\{class_hive}\\{filter_name}" /v "{filter_type}" /t REG_MULTI_SZ /d "{es_value}" /f'
-                        )
-                        break
+                        new.remove(driver)
 
-    ds_start_value = 0
-    for item in service_dump:
-        if read_value(f"{services_hive}\\{item}", "Start") is not None:
-            if item in automatic:
-                ds_start_value = 2
-            elif item in manual:
-                ds_start_value = 3
+                # check if original was modified at all
+                if original != new:
+                    ds_lines.append(
+                        f'reg.exe add "HKLM\\{class_hive}\\{filter_id}" /v "{filter_type}" /t REG_MULTI_SZ /d "{split_lines(new)}" /f'
+                    )
+                    es_lines.append(
+                        f'reg.exe add "HKLM\\{class_hive}\\{filter_id}" /v "{filter_type}" /t REG_MULTI_SZ /d "{split_lines(original)}" /f'
+                    )
+
+    for service in service_dump:
+        original_start_value = read_value(f"{services_hive}\\{service}", "Start")
+
+        if original_start_value is not None:
+            if service in automatic:
+                new_start_value = 2
+            elif service in manual:
+                new_start_value = 3
             else:
-                ds_start_value = 4
+                new_start_value = 4
+
             ds_lines.append(
-                f'Reg.exe add "HKLM\\{services_hive}\\{item}" /v "Start" /t REG_DWORD /d "{ds_start_value}" /f'
+                f'reg.exe add "HKLM\\{services_hive}\\{service}" /v "Start" /t REG_DWORD /d "{new_start_value}" /f'
             )
 
-            es_start_value = str(read_value(f"{services_hive}\\{item}", "Start"))
             es_lines.append(
-                f'Reg.exe add "HKLM\\{services_hive}\\{item}" /v "Start" /t REG_DWORD /d "{es_start_value}" /f'
+                f'reg.exe add "HKLM\\{services_hive}\\{service}" /v "Start" /t REG_DWORD /d "{original_start_value}" /f'
             )
 
     ds_lines.append("shutdown /r /f /t 0")

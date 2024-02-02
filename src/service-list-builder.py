531db071d387bba0a7ceac2e3c8a6eb00a4adc41
AMIT <86924251+amitxv@users.noreply.github.com>
2024-02-02 14:09:47 +0000

    Switch to ruff and upgrade to Python 3.12.1

--- src/service-list-builder.py
+++ src/service-list-builder.py
@@ -5,35 +5,41 @@ import sys
 import winreg
 from collections import deque
 from configparser import ConfigParser, SectionProxy
-from typing import Any, Deque, Dict, List, Set, Tuple, Union
+from typing import Any
 
 import win32service
 import win32serviceutil
-
 from constants import HIVE, USER_MODE_TYPES
 
 
-def null_terminator(array: List[str]) -> str:
-    return "\\0".join(array)
-
-
-def read_value(path: str, value_name: str) -> Union[Tuple[Any, int], None]:
+def read_value(path: str, value_name: str) -> Any | None:
+    """Read key values from Windows registry."""
     try:
-        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path, 0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY) as key:
+        with winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE,
+            path,
+            0,
+            winreg.KEY_READ | winreg.KEY_WOW64_64KEY,
+        ) as key:
             return winreg.QueryValueEx(key, value_name)[0]
     except FileNotFoundError:
         return None
 
 
-def get_dependencies(service: str, kernel_mode: bool = False) -> Set[str]:
-    dependencies: Union[List[str], None] = read_value(f"{HIVE}\\Services\\{service}", "DependOnService")
+def get_dependencies(service: str, kernel_mode: bool) -> set[str]:
+    """Return dependency tree for a given service."""
+    dependencies: list[str] | None = read_value(
+        f"{HIVE}\\Services\\{service}",
+        "DependOnService",
+    )
 
     # base case
     if dependencies is None or len(dependencies) == 0:
         return set()
 
     if not kernel_mode:
-        # remove kernel-mode services from dependencies list so we are left with user-mode dependencies only
+        # remove kernel-mode services from dependencies list so we are left with
+        # user-mode dependencies only
         dependencies = [
             dependency
             for dependency in dependencies
@@ -41,15 +47,21 @@ def get_dependencies(service: str, kernel_mode: bool = False) -> Set[str]:
         ]
 
     child_dependencies = {
-        child_dependency for dependency in dependencies for child_dependency in get_dependencies(dependency)
+        child_dependency
+        for dependency in dependencies
+        for child_dependency in get_dependencies(dependency, kernel_mode)
     }
 
     return set(dependencies).union(child_dependencies)
 
 
-def get_present_services() -> Dict[str, str]:
+def get_present_services() -> dict[str, str]:
+    """Get all installed services and return a dict[str, str].
+
+    Key is service name in lowercase and value is uppercase
+    """
     # keeps track of service in lowercase (key) and actual service name (value)
-    present_services: Dict[str, str] = {}
+    present_services: dict[str, str] = {}
 
     with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, f"{HIVE}\\Services") as key:
         num_subkeys = winreg.QueryInfoKey(key)[0]
@@ -66,7 +78,11 @@ def get_present_services() -> Dict[str, str]:
     return present_services
 
 
-def parse_config_list(service_list: SectionProxy, present_services: Dict[str, str]) -> Set[str]:
+def parse_config_list(
+    service_list: SectionProxy,
+    present_services: dict[str, str],
+) -> set[str]:
+    """Return a set of valid entries in the config."""
     return {
         present_services[lower_service]
         for service in service_list
@@ -74,12 +90,12 @@ def parse_config_list(service_list: SectionProxy, present_services: Dict[str, st
     }
 
 
-def main() -> int:
+def main() -> int:  # noqa: PLR0911, C901, PLR0912, D103, PLR0915
     version = "0.5.5"
     present_services = get_present_services()
 
     print(
-        f"service-list-builder Version {version} - GPLv3\nGitHub - https://github.com/amitxv\nDonate - https://www.buymeacoffee.com/amitxv\n"
+        f"service-list-builder Version {version} - GPLv3\nGitHub - https://github.com/amitxv\nDonate - https://www.buymeacoffee.com/amitxv\n",
     )
 
     if not ctypes.windll.shell32.IsUserAnAdmin():
@@ -140,7 +156,7 @@ def main() -> int:
         print(
             f"{service_name} has 0 dependencies"
             if len(dependencies) == 0
-            else f"{service_name} depends on {', '.join(dependencies)}"
+            else f"{service_name} depends on {', '.join(dependencies)}",
         )
         return 0
 
@@ -148,30 +164,40 @@ def main() -> int:
         print("error: config file not found")
         return 1
 
-    config = ConfigParser(allow_no_value=True, delimiters=("="), inline_comment_prefixes="#")
+    config = ConfigParser(
+        allow_no_value=True,
+        delimiters=("="),
+        inline_comment_prefixes="#",
+    )
     # prevent lists imported as lowercase
-    config.optionxform = str
+    config.optionxform = lambda optionstr: optionstr
     config.read(args.config)
 
     # load sections from config and handle case insensitive entries
     enabled_services = parse_config_list(config["enabled_services"], present_services)
-    service_dump = parse_config_list(config["individual_disabled_services"], present_services)
-    rename_binaries = set(binary for binary in config["rename_binaries"] if binary != "")
+    service_dump = parse_config_list(
+        config["individual_disabled_services"],
+        present_services,
+    )
+    rename_binaries = {binary for binary in config["rename_binaries"] if binary != ""}
 
     # check dependencies
     has_dependency_errors = False
 
     # required for lowercase comparison
-    lower_services_set: Set[str] = {service.lower() for service in enabled_services}
+    lower_services_set: set[str] = {service.lower() for service in enabled_services}
 
     for service in enabled_services:
         # get a set of the dependencies in lowercase
-        dependencies = set(service.lower() for service in get_dependencies(service))
+        dependencies = {
+            service.lower() for service in get_dependencies(service, kernel_mode=False)
+        }
 
         # check which dependencies are not in the user's list
         # then get the actual name from present_services as it was converted to lowercase to handle case inconsistency in Windows
         missing_dependencies = {
-            present_services[dependency] for dependency in dependencies.difference(lower_services_set)
+            present_services[dependency]
+            for dependency in dependencies.difference(lower_services_set)
         }
 
         if len(missing_dependencies) > 0:
@@ -183,20 +209,26 @@ def main() -> int:
 
     if enabled_services:
         # populate service_dump with all user mode services
-        for _, service_name in present_services.items():
-            service_type: int = read_value(f"{HIVE}\\Services\\{service_name}", "Type")
+        for service_name in present_services.values():
+            service_type = read_value(f"{HIVE}\\Services\\{service_name}", "Type")
 
-            if service_type in USER_MODE_TYPES:
-                service_dump.add(service_name)
+            if service_type is not None:
+                service_type = int(service_type)
+
+                if service_type in USER_MODE_TYPES:
+                    service_dump.add(service_name)
 
     if args.disable_running:
         for service in service_dump.copy():
-            if not win32serviceutil.QueryServiceStatus(service)[1] == win32service.SERVICE_RUNNING:
+            if (
+                not win32serviceutil.QueryServiceStatus(service)[1]
+                == win32service.SERVICE_RUNNING
+            ):
                 service_dump.remove(service)
 
     # store contents of batch scripts
-    ds_lines: Deque[str] = deque()
-    es_lines: Deque[str] = deque()
+    ds_lines: deque[str] = deque()
+    es_lines: deque[str] = deque()
 
     for binary in rename_binaries:
         if os.path.exists(binary):
@@ -220,7 +252,10 @@ def main() -> int:
             filter_id = winreg.EnumKey(key, i)
 
             for filter_type in ("LowerFilters", "UpperFilters"):
-                original: Union[List[str], None] = read_value(f"{HIVE}\\Control\\Class\\{filter_id}", filter_type)
+                original: list[str] | None = read_value(
+                    f"{HIVE}\\Control\\Class\\{filter_id}",
+                    filter_type,
+                )
 
                 # check if the filter exists
                 if original is not None:
@@ -232,10 +267,10 @@ def main() -> int:
                     # check if original was modified at all
                     if original != new:
                         ds_lines.append(
-                            f'reg.exe add "HKLM\\%HIVE%\\Control\\Class\\{filter_id}" /v "{filter_type}" /t REG_MULTI_SZ /d "{null_terminator(new)}" /f'
+                            f'reg.exe add "HKLM\\%HIVE%\\Control\\Class\\{filter_id}" /v "{filter_type}" /t REG_MULTI_SZ /d "{"\\0".join(new)}" /f',
                         )
                         es_lines.append(
-                            f'reg.exe add "HKLM\\%HIVE%\\Control\\Class\\{filter_id}" /v "{filter_type}" /t REG_MULTI_SZ /d "{null_terminator(original)}" /f'
+                            f'reg.exe add "HKLM\\%HIVE%\\Control\\Class\\{filter_id}" /v "{filter_type}" /t REG_MULTI_SZ /d "{"\\0".join(original)}" /f',
                         )
 
     for service in sorted(service_dump, key=str.lower):
@@ -243,11 +278,11 @@ def main() -> int:
 
         if original_start_value is not None:
             ds_lines.append(
-                f'reg.exe add "HKLM\\%HIVE%\\Services\\{service}" /v "Start" /t REG_DWORD /d "{original_start_value if service in enabled_services else 4}" /f'
+                f'reg.exe add "HKLM\\%HIVE%\\Services\\{service}" /v "Start" /t REG_DWORD /d "{original_start_value if service in enabled_services else 4}" /f',
             )
 
             es_lines.append(
-                f'reg.exe add "HKLM\\%HIVE%\\Services\\{service}" /v "Start" /t REG_DWORD /d "{original_start_value}" /f'
+                f'reg.exe add "HKLM\\%HIVE%\\Services\\{service}" /v "Start" /t REG_DWORD /d "{original_start_value}" /f',
             )
 
     if not ds_lines:

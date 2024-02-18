17b68ad812e402545e4cd416daf2d375fc9ed810
AMIT <86924251+amitxv@users.noreply.github.com>
2024-02-18 17:52:02 +0000

    Update version

--- src/service-list-builder.py
+++ src/service-list-builder.py
@@ -99,7 +99,7 @@ def get_file_metadata(file_path: str, attribute: str) -> str:
 
 
 def main() -> int:
-    version = "0.5.5"
+    version = "0.5.6"
     present_services = get_present_services()
 
     print(

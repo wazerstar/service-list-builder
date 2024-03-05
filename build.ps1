482ca86f31c36f344dbe3e91e13502f060bbbdae
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-05 10:19:21 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,8 +2,12 @@ function main() {
     # pack executable
     pyinstaller service_list_builder\main.py --onefile --name service-list-builder
 
+    if (Test-Path "building") {
+        Remove-Item -Path "building" -Recurse
+    }
+
     # create folder structure
-    mkdir building\service-list-builder
+    New-Item -ItemType Directory -Path building\service-list-builder
     Move-Item dist\service-list-builder.exe building\service-list-builder
     Move-Item service_list_builder\build building\service-list-builder
     Move-Item service_list_builder\lists.ini building\service-list-builder
